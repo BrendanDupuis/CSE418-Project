8e200cbb8d5902b4ab9ed7802c3198305fa0f897
BrendanDupuis <bjdupuis@buffalo.edu>
2025-11-03 13:36:54 -0500

    attack folder

--- attack/false_positives/08CWE762FalsePositive.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-int main() {
-    int *p = malloc(sizeof(int));
-    if (!p) return 1;  // check allocation success
-
-    free(p);
-    return 0;
-}
--- attack/false_positives/09CWE134FalseNeg.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int (*printer)(const char *, ...) = printf;
-int main(int argc, char **argv){
-    if(argc>1) printer(argv[1]); // user-controlled format through function pointer
-    return 0;
-}
